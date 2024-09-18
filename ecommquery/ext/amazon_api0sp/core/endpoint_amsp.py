49081f0556a1285c6283ffaea511a1213159d959
Barnaba Mateusz Piwek <barnaba@proximamail.net>
2024-09-18 17:10:35 +0200

    Amazon SP API under impl.

--- ecommquery/ext/amazon_api0sp/core/endpoint_amsp.py
+++ ecommquery/ext/amazon_api0sp/core/endpoint_amsp.py
@@ -2,6 +2,7 @@ from ecommquery.ecommquery import Mode
 from ecommquery.core.validators import RegExValidator
 
 from ecommquery.core.endpoint import Endpoint
+from ecommquery.ext.amazon_api0sp.core.service_amsp import ServiceAmSP
 
 
 # Amazon Selling Partnr API
@@ -18,8 +19,11 @@ class EndpointAmSP(Endpoint):
     def __init__(self, params : {}):
         # This should validate Amazon Client ID and secret key
         am_validator = RegExValidator('[a-z0-9|\.|\-]{16,96}')
+        rt_validator = RegExValidator('[a-zA-Z0-9|_|\-|\|]{128,512}')
+
         super().__init__({'client_id': Endpoint.Constr('_client_id', am_validator),
-                          'client_secret_key': Endpoint.Constr('_client_secret_key', am_validator)},
+                          'client_secret_key': Endpoint.Constr('_client_secret_key', am_validator),
+                          'refresh_token': Endpoint.Constr('_refresh_token', rt_validator)},
                         params)
 
     def info(self):
@@ -29,7 +33,7 @@ class EndpointAmSP(Endpoint):
         return info
 
     def _getService(self, mode: Mode):
-        return None
+        return ServiceAmSP( self._client_id, self._client_secret_key, **Mode.as_args(mode) )
 
 Endpoint.register(EndpointAmSP)
 
