a71c33bb413412c904d107c8b3662f32bce58da6
Mateusz Piwek <barnaba@200ms.net>
2026-01-03 18:28:50 +0100

    WIP

--- README-datastructure.md
+++ README-datastructure.md
@@ -1,4 +1,60 @@
-# MISO DB
+# MISOt DB
 
-Minimalist inter-sected object database with time keeping
+Minimalist inter-sected object database (with time and event logging)
+
+Multi-parameter multi-data source database structure with event logging
+
+
+## Concept
+
+This database structure has been developed to handle objects that has: 
+
+- multiple parameters
+- parameters are set using multiple various sources
+
+database also does: 
+
+- trace the source of all parameters
+- keeps change history
+- annotates what caused a certain action
+
+
+This is to handle scenario that happen in [ecommquery](https://github.com/tools200ms/ecommquery) framework. The framework has been developed for intenet marchants In this case, the object is a product that is on intentet sale. Each product has multiple parameters, such as, name, EAN code, price, stock, weight etc..
+
+One product can be sold on multiple platforms, hence multiple parameters are feed by multiple sources: auction sites, internet stores.
+
+Each parameter is annotated to trace back what was the source for a given value (in this case what internet store). Moreover, we need to call certain actions upon a certain changes (such as stock change). Hance this database structure keeps track of what method (eg. Python method) has been invokd to process given update.
+
+This database structure is generic, therefore can be applied to another cases. 
+
+- multiple object parameters
+- multiple data sources
+- need to trace changes
+- need to annotate what actions has been called
+
+Belo, deails about structre are presented: 
+
+## Structure
+
+At the top there is a `space`, the space is divided into portions - such as disk.
+
+There might be mulitple speces defined, and multiple partitions within space. There is no sub-partitions, nor sub-spaces.
+
+There is a property set defined, property can be global, or bound to a certain space.
+
+All data updates are preceded by `object checkout`. `Object checkout` has its source, that is defined in `Object checkout sources` and has a scope limited to a given partition. Also, it provides reference toPython mehod invoked.
+
+First, `Object chackuot` is called - that comes always with call of a spectifi Python mehod. After, a certain properties can be updated - if a new value is found.
+
+Object is composed of parameters, parameters can be global, or space bound.
+
+**The key concept is a fact that object can belong to multiple partions, or in other words. be characterised by having properties from diffrent spaces.**
+
+Importantl, nothing is removed, all changes are annotated - thus it is posigble to trace what made a given change.
+
+# Summary
+
+
+This database stucture was designed to make it as simmple as possible, but still to provide comprehensive data handling. The structure is below 10 tables.
+
 
--- ecommquery/db/schema.sql
+++ ecommquery/db/schema.sql
@@ -9,15 +9,6 @@ CREATE TABLE comments (
 INSERT INTO comments (msg)
     VALUES ('Database has been created');
 
-DROP TABLE IF EXISTS prop_def;
-CREATE TABLE prop_def (
-    id CHAR(8) NOT NULL,
-    ref_name varchar(255),
-    type CHAR,
-    validation_pattern varchar(255),
-    flags CHAR, -- R: reference, P: property
-    PRIMARY KEY (id)
-);
 
 -- Initialise dictionary ...
 
@@ -29,76 +20,91 @@ CREATE TABLE ns_def (
     UNIQUE (id, name)
 );
 
-DROP TABLE IF EXISTS ns_prop_def;
-CREATE TABLE ns_prop_def (
-    ns_id CHAR(8),
-    prop_id SMALLINT,
-    name VARCHAR(512),
-    PRIMARY KEY (ns_id, prop_id),
-    FOREIGN KEY (ns_id) REFERENCES ns_def(id),
-    FOREIGN KEY (prop_id) REFERENCES prop_def(id)
-);
+DROP TABLE IF EXISTS part_def;
+CREATE TABLE part_def {
+    id SMALLINT NOT NULL,
+    spc_id SMALLINT,
+    name CHAR(16),
+    PRIMARY KEY (id),
+    UNIQUE (id, spc_id, name)
+}
 
--- CREATE TABLE ep_item_props (
---     id CHAR(16),
---     prop_id SMALLINT,
---     PRIMARY KEY (id, prop_id),
---     FOREIGN KEY (prop_id) REFERENCES item_prop_def(id)
--- );
+DROP TABLE IF EXISTS prop_def;
+CREATE TABLE prop_def (
+    id CHAR(8) NOT NULL,
+    ns_id CHAR(8) DEFAULT NULL,
+    ref_name varchar(255),
+    type CHAR,
+    validation_pattern varchar(255),
+    flags CHAR, -- R: reference, P: property
+    PRIMARY KEY (id)
+);
+-- EAN
+-- Name
+-- Detailed name
+
+--DROP TABLE IF EXISTS ns_prop_def;
+--CREATE TABLE ns_prop_def (
+--    ns_id CHAR(8),
+--    prop_id SMALLINT,
+--    name VARCHAR(512),
+--    PRIMARY KEY (ns_id, prop_id),
+--    UNIQUE prop_id,
+--    FOREIGN KEY (ns_id) REFERENCES ns_def(id),
+--    FOREIGN KEY (prop_id) REFERENCES prop_def(id)
+--);
+-- ProdID (Presta)
+-- OferID (Allegro)
+
 
 -- END of definitions
 
--- CREATE TABLE part_def (
---     id SMALLINT NOT NULL AUTO_INCREMENT,
---     name CHAR(256),
---     ns_id SMALLINT,
---     PRIMARY KEY (id),
---     FOREIGN KEY (ns_id) REFERENCES ns_def(id),
---     UNIQUE (id, name)
--- );
 
-DROP TABLE IF EXISTS part_prop_def;
-CREATE TABLE part_prop_def (
-    part_id SMALLINT,
-    prop_id SMALLINT,
-    --excl_prop_id SMALLINT,
-    FOREIGN KEY (part_id) REFERENCES part_def(id),
-    FOREIGN KEY (prop_id) REFERENCES ns_prop_def(id)
-    --FOREIGN KEY (excl_prop_id) REFERENCES ns_prop_def(id)
-);
+--DROP TABLE IF EXISTS part_prop_def;
+--CREATE TABLE part_prop_def (
+--    part_id SMALLINT,
+--    prop_id SMALLINT,
+--    --excl_prop_id SMALLINT,
+--    FOREIGN KEY (part_id) REFERENCES part_def(id),
+--    FOREIGN KEY (prop_id) REFERENCES ns_prop_def(id)
+--    --FOREIGN KEY (excl_prop_id) REFERENCES ns_prop_def(id)
+--);
 
 -- Object can be cloned into other set
-DROP TABLE IF EXISTS obj;
-CREATE TABLE obj (
-    id INTEGER NOT NULL,
-    part_id SMALLINT,
-    PRIMARY KEY (id, part_id),
-    FOREIGN KEY (part_id) REFERENCES ns_prop_def(id)
-);
+--DROP TABLE IF EXISTS obj;
+--CREATE TABLE obj (
+--    id INTEGER NOT NULL,
+--    --part_id SMALLINT,
+--    PRIMARY KEY (id),
+--    --FOREIGN KEY (part_id) REFERENCES ns_prop_def(id)
+--);
 
-DROP TABLE IF EXISTS obj_checkout_sources;
+DROP TABLE IF EXISTS checkout_sources_def;
 CREATE TABLE obj_checkout_sources (
     id SMALLINT NOT NULL,
     name VARCHAR(256),
-    params VARCHAR(4096),
+    part_id_scope SMALLINT NOT NULL,
+    function VARCHAR,
+    triggered_by SMALLINT DEFAULT NULL,
     PRIMARY KEY (id)
 );
 
 DROP TABLE IF EXISTS obj_checkout;
 CREATE TABLE obj_checkout (
     id INTEGER NOT NULL,
+    --part_id SMALLINT,
     obj_id INTEGER NOT NULL,
-    since DATETIME DEFAULT (datetime('now', 'localtime')),
-    till DATETIME DEFAULT NULL,
+    on DATETIME DEFAULT (datetime('now', 'localtime')),
+    --till DATETIME DEFAULT NULL,
     triggered_by SMALLINT,
     -- correction, transaction
-    PRIMARY KEY (id),
-    FOREIGN KEY (obj_id) REFERENCES obj(id)
+    PRIMARY KEY (id, obj_id)
+    --FOREIGN KEY (obj_id) REFERENCES obj(id)
 );
 
 DROP TABLE IF EXISTS obj_prop_nochange;
 CREATE TABLE obj_prop_nochange (
-    obj_id INTEGER,
+    --obj_id INTEGER,
     prop_id SMALLINT,
     checkout_id INTEGER,
     PRIMARY KEY (obj_id, prop_id),
@@ -109,7 +115,7 @@ CREATE TABLE obj_prop_nochange (
 
 DROP TABLE IF EXISTS obj_prop_text;
 CREATE TABLE obj_prop_text (
-    obj_id INTEGER,
+    --obj_id INTEGER,
     prop_id SMALLINT,
     checkout_id INTEGER,
     value VARCHAR(4096),
@@ -121,7 +127,7 @@ CREATE TABLE obj_prop_text (
 
 DROP TABLE IF EXISTS obj_prop_int;
 CREATE TABLE obj_prop_int (
-    obj_id INTEGER,
+    --obj_id INTEGER,
     prop_id SMALLINT,
     checkout_id INTEGER,
     value INTEGER,
